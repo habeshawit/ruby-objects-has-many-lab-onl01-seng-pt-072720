90abf2a73c30c37af8304d893e66d8c1770fa0e7
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-21 08:28:44 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,4 +1,22 @@
 class Post 
-  attr_accessor :author
+  attr_accessor :author, :title
   
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    @@all << title 
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def author_name
+    if author !=nil 
+      self.author = author.name 
+    else
+      return nil
+    end
+  end
 end
