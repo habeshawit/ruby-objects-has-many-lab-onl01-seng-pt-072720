31b60f3bd706f220d7c23cf58cd0874c962448ab
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-21 08:35:06 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -19,4 +19,23 @@ class Post
       return nil
     end
   end
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    if artist == nil
+      return nil 
+    else
+      self.artist = artist.name
+    end
+  end
 end
